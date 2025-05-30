c5e698503b1796453a575a7edbb340cc3c507dd5
Shanty <163443786+ShantyCerdasB@users.noreply.github.com>
2025-05-29 23:56:55 -0600

    fixing

--- web/src/components/ui/header.tsx
+++ web/src/components/ui/header.tsx
@@ -84,13 +84,7 @@ export default function Header() {
                   to="/reports/top-products"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600"
                 >
-                  Top Products
-                </Link>
-                <Link
-                  to="/reports/top-clients"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600"
-                >
-                  Top Clients
+                  Reports Top Products and Clients
                 </Link>
               </div>
             </PopoverPanel>
